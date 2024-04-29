2f87d11a3bef7b42c5c046b84b97c4185fe92eec
miz8462 <wwr8462@gmail.com>
2024-04-29 16:16:21 +0900

    dotenv をインストール .env ファイルを作成し、main.dartでsupabaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kimikoe_app/kimikoe_app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const ProviderScope(child: KimikoeApp()));
 }
+
+final supabase = Supabase.instance.client;
