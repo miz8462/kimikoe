ed462e646708cfd04166c1ef4698de0c2772b0d0
miz8462 <wwr8462@gmail.com>
2024-04-29 16:50:10 +0900

    main.dart にメインのアプリケーションを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kimikoe_app/kimikoe_app.dart';
+import 'package:kimikoe_app/ui/auth/view/sign_in_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  // Flutterの初期化
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
 
+  // Supabaseの初期化
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  // Riverpodでラップ
   runApp(const ProviderScope(child: KimikoeApp()));
 }
-
+// Supabaseクライアントを取得
 final supabase = Supabase.instance.client;
+
+class KimikoeApp extends StatelessWidget {
+  const KimikoeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'キミコエ',
+      home: SignInPage(),
+    );
+  }
+}
