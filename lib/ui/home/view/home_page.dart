066d18cddf3f03045886373c6dd03b0992a17df8
miz8462 <wwr8462@gmail.com>
2024-05-09 17:18:58 +0900

    ホームでグループ一覧をグリッド状に表示

--- lib/ui/home/view/home_page.dart
+++ lib/ui/home/view/home_page.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:kimikoe_app/config/config.dart';
+
+/*
+グループ一覧を表示する
+画面上はグリッドで横2列、縦4行（？）
+下方にスクロールしてさらにグループを表示
+各グループをカードで画像と名前を表示する
+*/
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Homeページ'),);
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+      ),
+      itemCount: 8,
+      itemBuilder: (context, index) {
+        return GroupCard();
+      },
+    );
   }
 }
+
+class GroupCard extends StatelessWidget {
+  const GroupCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/images/poison_palette.jpg'),
+            ),
+            Text(
+              'Poison Palette',
+              style: TextStyle(
+                color: textDark,
+              ),
+            ),
+          ],
+        ),
+      );
+  }
+}
+
