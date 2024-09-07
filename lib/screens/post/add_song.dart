45875a8f3268eccf578b312d04857c6f88af341d
miz8462 <wwr8462@gmail.com>
2024-09-07 23:13:28 +0900

    歌詞登録機能の実装（要改善改善）

--- lib/screens/post/add_song.dart
+++ lib/screens/post/add_song.dart
@@ -1,18 +1,24 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
+    as picker;
 import 'package:gap/gap.dart';
 import 'package:kimikoe_app/config/config.dart';
+import 'package:kimikoe_app/main.dart';
 import 'package:kimikoe_app/models/idol_group.dart';
+import 'package:go_router/go_router.dart';
+
 import 'package:kimikoe_app/screens/appbar/top_bar.dart';
 import 'package:kimikoe_app/utils/create_image_name_with_jpg.dart';
 import 'package:kimikoe_app/utils/dropdown_menu_group_list.dart';
 import 'package:kimikoe_app/utils/fetch_data.dart';
+import 'package:kimikoe_app/utils/formatter.dart';
 import 'package:kimikoe_app/utils/validator/validator.dart';
 import 'package:kimikoe_app/widgets/buttons/image_input_button.dart';
 import 'package:kimikoe_app/widgets/buttons/styled_button.dart';
+import 'package:kimikoe_app/widgets/forms/drum_roll_form.dart';
 import 'package:kimikoe_app/widgets/forms/expanded_text_form.dart';
-import 'package:kimikoe_app/widgets/forms/text_form.dart';
 import 'package:kimikoe_app/widgets/forms/text_input_form.dart';
 
 class AddSongScreen extends StatefulWidget {
@@ -25,11 +31,14 @@ class AddSongScreen extends StatefulWidget {
 class _AddSongScreenState extends State<AddSongScreen> {
   final _formKey = GlobalKey<FormState>();
 
+  final _releaseDateController = TextEditingController();
+
   var _enteredTitle = '';
   IdolGroup? _selectedGroup;
   File? _selectedImage;
   var _enteredLyricist = '';
   var _enteredComposer = '';
+  String? _selectedReleaseDate;
   var _enteredLyric = '';
   var _enteredComment = '';
 
@@ -59,7 +68,39 @@ class _AddSongScreenState extends State<AddSongScreen> {
     // e.g. /aaa/bbb/ccc/image.png
     final imagePathWithCreatedAtJPG = createImageNameWithJPG(_selectedImage);
 
-    // todo: 作詞家や作曲家が登録されてない場合は名前だけ自動的に登録されるようにする
+    if (_selectedReleaseDate == null || _selectedReleaseDate!.isEmpty) {
+      _selectedReleaseDate = null;
+    }
+
+    await await supabase.from('songs').insert({
+      'title': _enteredTitle,
+      'lyrics': _enteredLyric,
+      'group_id': _selectedGroup?.id == null ? null : _selectedGroup!.id,
+      'image_url': _selectedImage == null
+          ? defaultPathNoImage
+          : imagePathWithCreatedAtJPG,
+      'release_date': _selectedReleaseDate,
+      'lyricist': _enteredLyricist,
+      'composer': _enteredComposer,
+      'comment': _enteredComment,
+    });
+
+    if (_selectedImage != null) {
+      await supabase.storage
+          .from('images')
+          .upload(imagePathWithCreatedAtJPG!, _selectedImage!);
+    }
+
+    setState(() {
+      _isSending = false;
+    });
+
+    if (!mounted) {
+      return;
+    }
+
+    context.pushReplacement('/group_list');
+    // todo: 作詞家や作曲家が登録されてない場合は名前だけartistテーブルに自動的に登録されるようにする
   }
 
   String? _titleValidator(String? value) {
@@ -72,6 +113,24 @@ class _AddSongScreenState extends State<AddSongScreen> {
 
   String? _composerValidator(String? value) {
     return textInputValidator(value, '作曲家');
+  }
+
+  void _pickReleaseDate() async {
+    await picker.DatePicker.showDatePicker(
+      context,
+      showTitleActions: false,
+      minTime: DateTime(1900),
+      maxTime: DateTime.now(),
+      currentTime: DateTime(2020, 6, 15),
+      locale: picker.LocaleType.jp,
+      onChanged: (date) {
+        _selectedReleaseDate = date.toIso8601String();
+        _releaseDateController.text = formatDateTimeToXXXX(
+          date: date,
+          formatStyle: 'yyyy/MM/dd',
+        );
+      },
+    );
   }
 
   @override
@@ -101,6 +160,15 @@ class _AddSongScreenState extends State<AddSongScreen> {
                   },
                 ),
                 Gap(spaceWidthS),
+                ExpandedTextForm(
+                  onTextChanged: (value) {
+                    setState(() {
+                      _enteredLyric = value!;
+                    });
+                  },
+                  label: '歌詞',
+                ),
+                Gap(spaceWidthS),
                 DropdownMenuGroupList(
                   onGroupSelected: (value) {
                     _selectedGroup = value;
@@ -112,7 +180,7 @@ class _AddSongScreenState extends State<AddSongScreen> {
                   onPickImage: (image) {
                     _selectedImage = image;
                   },
-                  label: 'イメージ画',
+                  label: 'イメージ画像',
                 ),
                 Gap(spaceWidthS),
                 InputForm(
@@ -131,16 +199,19 @@ class _AddSongScreenState extends State<AddSongScreen> {
                   },
                 ),
                 Gap(spaceWidthS),
-                TextForm(hintText: '発売日'),
-                Gap(spaceWidthS),
-                ExpandedTextForm(
-                  onTextChanged: (value) {
-                    setState(() {
-                      _enteredLyric = value!;
-                    });
+                PickerForm(
+                  label: '発売日',
+                  controller: _releaseDateController,
+                  picker: _pickReleaseDate,
+                  onSaved: (value) {
+                    setState(
+                      () {
+                        _selectedReleaseDate = value!;
+                      },
+                    );
                   },
-                  label: '歌詞',
                 ),
+                Gap(spaceWidthS),
                 Gap(spaceWidthS),
                 ExpandedTextForm(
                   onTextChanged: (value) {
