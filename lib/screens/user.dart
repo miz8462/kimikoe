56ed10905c10beef5a94461ddc0df8bbb9e1f1cf
miz8462 <wwr8462@gmail.com>
2024-09-08 11:18:26 +0900

    UserScreenのデータをプロバイダーから取得するよう変更

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kimikoe_app/config/config.dart';
 import 'package:kimikoe_app/main.dart';
 import 'package:kimikoe_app/models/user.dart';
+import 'package:kimikoe_app/provider/current_user.dart';
 import 'package:kimikoe_app/router/routing_path.dart';
 import 'package:kimikoe_app/screens/appbar/top_bar.dart';
-import 'package:kimikoe_app/utils/fetch_data.dart';
 import 'package:kimikoe_app/widgets/buttons/styled_button.dart';
 
-class UserScreen extends StatefulWidget {
+class UserScreen extends ConsumerStatefulWidget {
   const UserScreen({super.key});
 
   @override
-  State<UserScreen> createState() => _UserScreenState();
+  ConsumerState<UserScreen> createState() => _UserScreenState();
 }
 
-class _UserScreenState extends State<UserScreen> {
-  late Future _currentUserInfo;
+class _UserScreenState extends ConsumerState<UserScreen> {
+  late AsyncValue<List<Map<String, dynamic>>> _currentUserInfo;
 
   @override
-  void initState() {
-    super.initState();
-    _currentUserInfo = fetchCurrentUserInfo();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // ref.watchはbuildメソッドやdidChangeDependenciesメソッド内で使用する必要がある
+    _currentUserInfo = ref.watch(userDataProvider);
   }
 
   String _getImageFromSupabase(String imageUrl) {
@@ -33,78 +35,69 @@ class _UserScreenState extends State<UserScreen> {
   Widget build(context) {
     const String editButtonText = '編集する';
     const double buttonWidth = 180;
+    final currentUserInfo =
+        _currentUserInfo.value?.map((data) => data).toList()[0];
+    final user = UserProfile(
+      name: currentUserInfo?['username'] ?? 'タイトル未定',
+      email: currentUserInfo?['email'],
+      imageUrl: currentUserInfo?['image_url'],
+      comment: currentUserInfo?['comment'] ?? '',
+    );
+
+    var userImageUrl = user.imageUrl;
+    final isStartWithHTTP = user.imageUrl.startsWith('http');
+
+    if (!isStartWithHTTP) {
+      userImageUrl = _getImageFromSupabase(user.imageUrl);
+    }
+    var userImage = NetworkImage(userImageUrl);
 
     return Scaffold(
       appBar: TopBar(
         title: 'ユーザー情報',
       ),
-      body: FutureBuilder(
-        future: _currentUserInfo,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          }
-          final userDataInList = List.of(snapshot.data);
-          final userData = userDataInList[0];
-          final user = UserProfile(
-            name: userData['username'] ?? 'タイトル未定',
-            email: userData['email'],
-            imageUrl: userData['image_url'],
-            comment: userData['comment'] ?? '',
-          );
-
-          var userImageUrl = user.imageUrl;
-          final isStartWithHTTP = user.imageUrl.startsWith('http');
-
-          if (!isStartWithHTTP) {
-            userImageUrl = _getImageFromSupabase(user.imageUrl);
-          }
-          var userImage = NetworkImage(userImageUrl);
-
-          return Padding(
-            padding: screenPadding,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Padding(
+        padding: screenPadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(spaceWidthS),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Gap(spaceWidthS),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: userImage,
-                      radius: 30,
-                    ),
-                    // 編集
-                    // todo: 他のページに遷移したら編集を終了する
-                    StyledButton(
-                      editButtonText,
-                      onPressed: () {
-                        context.push(
-                            '${RoutingPath.userDetails}/${RoutingPath.editUser}',
-                            extra: user);
-                      },
-                      textColor: textGray,
-                      backgroundColor: backgroundWhite,
-                      buttonSize: buttonM,
-                      borderSide: BorderSide(
-                          color: backgroundLightBlue, width: borderWidth),
-                      width: buttonWidth,
-                    ),
-                  ],
+                CircleAvatar(
+                  backgroundImage: userImage,
+                  radius: 30,
                 ),
-                Gap(spaceWidthS),
-                Text(
-                  user.name,
-                  style: TextStyle(
-                    fontSize: fontLL,
-                  ),
+                // 編集
+                // todo: 他のページに遷移したら編集を終了する
+                StyledButton(
+                  editButtonText,
+                  onPressed: () {
+                    context.push(
+                        '${RoutingPath.userDetails}/${RoutingPath.editUser}',
+                        extra: user);
+                  },
+                  textColor: textGray,
+                  backgroundColor: backgroundWhite,
+                  buttonSize: buttonM,
+                  borderSide: BorderSide(
+                      color: backgroundLightBlue, width: borderWidth),
+                  width: buttonWidth,
                 ),
-                Gap(spaceWidthS),
-                Text(user.comment),
               ],
             ),
-          );
-        },
+            Gap(spaceWidthS),
+            Text(
+              user.name,
+              style: TextStyle(
+                fontSize: fontLL,
+              ),
+            ),
+            Gap(spaceWidthS),
+            Text(user.comment),
+          ],
+        ),
       ),
     );
   }
