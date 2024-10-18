23dd3ca1778ebc2c083338cc9432c4ae0b0dc84b
miz8462 <wwr8462@gmail.com>
2024-10-18 16:00:33 +0900

    idolGroupListをプロバイダーで管理。

--- lib/providers/idol_groups_providere.dart
+++ lib/providers/idol_groups_providere.dart
@@ -1,12 +1,42 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kimikoe_app/models/enums/table_and_column_name.dart';
+import 'package:kimikoe_app/models/idol_group.dart';
 import 'package:kimikoe_app/utils/crud_data.dart';
 
+class IdolGroupListNotifier extends StateNotifier<List<IdolGroup>> {
+  IdolGroupListNotifier(super.state);
+
+  void addGroup(IdolGroup group) {
+    state = [...state, group];
+  }
+
+  void removeGroup(IdolGroup group) {
+    state = state.where((g) => g.id != group.id).toList();
+  }
+}
+
+final idolGroupListProvider =
+    StateNotifierProvider<IdolGroupListNotifier, List<IdolGroup>>((ref) {
+  final asyncValue = ref.watch(idolGroupListFromSupabaseProvider);
+  return asyncValue.maybeWhen(
+      data: (data) => IdolGroupListNotifier(data),
+      orElse: () => IdolGroupListNotifier([]));
+});
+
 final idolGroupListFromSupabaseProvider =
-    FutureProvider<List<dynamic>>((ref) async {
+    FutureProvider<List<IdolGroup>>((ref) async {
   final data = await fetchDatabyStream(
     table: TableName.idolGroups.name,
     id: ColumnName.id.name,
   ).first;
-  return data.toList();
+  return data.map<IdolGroup>((group) {
+    final imageUrl = fetchPublicImageUrl(group[ColumnName.imageUrl.name]);
+    return IdolGroup(
+      id: group[ColumnName.id.name],
+      name: group[ColumnName.cName.name],
+      imageUrl: imageUrl,
+      year: group[ColumnName.yearFormingGroups.name],
+      comment: group[ColumnName.comment.name],
+    );
+  }).toList();
 });
--- lib/screens/idol_group_list.dart
+++ lib/screens/idol_group_list.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kimikoe_app/models/enums/table_and_column_name.dart';
-import 'package:kimikoe_app/models/idol_group.dart';
 import 'package:kimikoe_app/providers/idol_groups_providere.dart';
 import 'package:kimikoe_app/screens/appbar/top_bar.dart';
-import 'package:kimikoe_app/utils/crud_data.dart';
 import 'package:kimikoe_app/widgets/group_card_l.dart';
 
 class IdolGroupListScreen extends ConsumerWidget {
@@ -12,47 +9,35 @@ class IdolGroupListScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final groupFuture = ref.watch(idolGroupListFromSupabaseProvider);
-    print(groupFuture);
-    print(groupFuture.runtimeType);
+    final asyncValue = ref.watch(idolGroupListFromSupabaseProvider);
+    final idolGroupList = ref.watch(idolGroupListProvider);
 
     return Scaffold(
       appBar: const TopBar(
         imageUrl: 'assets/images/Kimikoe_Logo.png',
         showLeading: false,
       ),
-      body: groupFuture.when(
-        data: (List<dynamic> groups) {
-          if (groups.isEmpty) {
-            return const Center(
-              child: Text('登録データはありません'),
-            );
-          }
-          return GridView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // 横に表示する数
-              crossAxisSpacing: 18, // 横のスペース
-              mainAxisSpacing: 15, // 縦のスペース
-              childAspectRatio: 1.0, // カードのアスペクト比
-            ),
-            itemCount: groups.length,
-            itemBuilder: (BuildContext context, int index) {
-              // IdolGroupクラスを初期化
-              final group = groups[index];
-              final imageUrl =
-                  fetchPublicImageUrl(group[ColumnName.imageUrl.name]);
-              final groupInfo = IdolGroup(
-                id: group[ColumnName.id.name],
-                name: group[ColumnName.cName.name],
-                imageUrl: imageUrl,
-                year: group[ColumnName.yearFormingGroups.name],
-                comment: group[ColumnName.comment.name],
-              );
-
-              return GroupCardL(group: groupInfo);
-            },
-          );
+      body: asyncValue.when(
+        data: (_) {
+          return idolGroupList.isEmpty
+              ? const Center(
+                  child: Text('登録データはありません'),
+                )
+              : GridView.builder(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // 横に表示する数
+                    crossAxisSpacing: 18, // 横のスペース
+                    mainAxisSpacing: 15, // 縦のスペース
+                    childAspectRatio: 1.0, // カードのアスペクト比
+                  ),
+                  itemCount: idolGroupList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final groupInfo = idolGroupList[index];
+                    return GroupCardL(group: groupInfo);
+                  },
+                );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (error, stack) => Center(child: Text('Error: $error')),
