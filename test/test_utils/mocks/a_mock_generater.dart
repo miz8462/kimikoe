0551b58e28f073da7cf1d5295af33a394d2ae5b9
miz8462 <wwr8462@gmail.com>
2024-12-13 08:59:36 +0900

    test: ImageInputウィジェット - MockImagePickerServiceを作成

--- test/test_utils/mocks/a_mock_generater.dart
+++ test/test_utils/mocks/a_mock_generater.dart
@@ -1,5 +1,5 @@
-import 'package:http/http.dart' as http;
+import 'package:kimikoe_app/services/image_picker_service.dart';
 import 'package:mockito/annotations.dart';
 
-@GenerateNiceMocks([MockSpec<http.Client>()])
+@GenerateNiceMocks([MockSpec<ImagePickerService>()])
 void main() {}
