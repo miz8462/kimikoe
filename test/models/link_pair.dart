edacd2efda41d31a379c14ed13b7d4a7f5f29ef8
miz8462 <wwr8462@gmail.com>
2024-12-12 11:01:14 +0900

    test: LinkPairクラス

--- test/models/link_pair.dart
+++
@@ -1,6 +0,0 @@
-class LinkPair {
-  const LinkPair({this.webUrl, this.deepLinkUrl});
-
-  final Uri? webUrl;
-  final Uri? deepLinkUrl;
-}
---
+++ test/models/link_pair_test.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+
+class LinkPair {
+  const LinkPair({this.webUrl, this.deepLinkUrl});
+
+  final Uri? webUrl;
+  final Uri? deepLinkUrl;
+}
+
+void main() {
+  test('LinkPairクラス', () {
+    final linkPair = LinkPair(
+      webUrl: Uri.parse('https://example.com'),
+      deepLinkUrl: Uri.parse('app://deep-link'),
+    );
+
+    expect(linkPair.webUrl, Uri.parse('https://example.com'));
+    expect(linkPair.deepLinkUrl, Uri.parse('app://deep-link'));
+  });
+}
