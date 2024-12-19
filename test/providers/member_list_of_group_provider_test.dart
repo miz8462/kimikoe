897dce8f3c9b4d8f761c121da145b539cf5c9013
miz8462 <wwr8462@gmail.com>
2024-12-19 11:46:21 +0900

    chore: 使ってないコードをコメントアウト

--- test/providers/member_list_of_group_provider_test.dart
+++ test/providers/member_list_of_group_provider_test.dart
@@ -1,13 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kimikoe_app/providers/member_list_of_group_provider.dart';
-
-import '../test_utils/mocks/logger_mock.dart';
 
 void main() {
-  group('MemberListInGroupNotifier', (){
-    late MockLogger mockLogger;
-    late MemberListInGroupNotifier notifier;
-
-    
+  group('MemberListInGroupNotifier', () {
+    // late MockLogger mockLogger;
+    // late MemberListInGroupNotifier notifier;
   });
 }
