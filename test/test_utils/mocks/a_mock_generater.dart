65782649a20af20867ff038d45a8a0a28c2ba38a
miz8462 <wwr8462@gmail.com>
2024-12-24 18:19:53 +0900

    chore: 不要ファイルの削除

--- test/test_utils/mocks/a_mock_generater.dart
+++ test/test_utils/mocks/a_mock_generater.dart
@@ -2,5 +2,6 @@ import 'package:mockito/annotations.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 @GenerateNiceMocks(
-    [MockSpec<PostgrestFilterBuilder<List<Map<String, dynamic>>>>()])
+  [MockSpec<PostgrestFilterBuilder<List<Map<String, dynamic>>>>()],
+)
 void main() {}
