adbf0d99449fac153e01238a3cc29742b3b05d6b
miz8462 <wwr8462@gmail.com>
2025-01-09 23:17:16 +0900

    test: ユーザーデータ削除処理を管理者用に変更

--- integration_test/auth/sign_up_test.dart
+++ integration_test/auth/sign_up_test.dart
@@ -38,7 +38,7 @@ void main() async {
     await robot.expectHomeScreen();
 
     // テストデータ削除
-    await robot.deleteUser();
-    // await robot.deleteUserAdmin();
+    // await robot.deleteUser();
+    await robot.deleteUserAdmin();
   });
 }
